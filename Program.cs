747b91148ddecce6a426142bc20261a56b8d9f79
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-08 16:39:55 +1300

    done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = "";
+            var input2 = "";
+
+
+            Console.WriteLine("Please enter your first name then press enter");
+            input1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name then press enter");
+            input2 = Console.ReadLine();
+
+
+            Console.WriteLine($" {input1 }   {input2}");
+
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            //done//
+
+        }
+    }
+}
